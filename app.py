59228f366b68c957d65434079e13611c8ec0154a
suhasuba246-design <suhasuba246@gmail.com>
2026-09-24 21:47:46 +0530

    syntax error fixed

--- app.py
+++ app.py
@@ -1,4 +1,1 @@
-st.subheader("🚨 Anomaly Detection...")
-...
-model = IsolationForest(contamination=0.05, random_state=42)  <- ITHU THAAN DA!
-preds = model.fit_predict(...)
+model = IsolationForest(contamination=0.05, random_state=42)
